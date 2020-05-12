76e74000745c8496a51cbc9106c5e2b38cb0f668
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-05-12 17:46:05 +0300

    task3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Customerlist from './components/Customerlist';
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Traininglist from "./components/Traininglist";
+import Calendar from "./components/Calendar";
 
 
 function App() {
@@ -10,14 +11,14 @@ function App() {
     <div>
       <BrowserRouter>
         <div>
-          
           <Link to= "/customerlist">Customerlist</Link>{''}
           <Link to= "/traininglist">Traininglist</Link>{''}
+          <Link to="/calendar">Calendar</Link>{''}
           <Switch>
-           
+            <Route path="/calendar" component={Calendar}/>
             <Route path="/customerlist" component={Customerlist}/>
             <Route path="/traininglist" component={Traininglist} />
-            <Route render={() => <h1>Page not found</h1>}/>		
+            {/*<Route render={() => <h1>Page not found</h1>}/>		*/}
           </Switch>
         </div>
       </BrowserRouter>
