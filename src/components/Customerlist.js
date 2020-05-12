34ecd038226b6f641fdbb22a7831ee4f4fb945b0
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-05-12 12:19:37 +0300

    Task2

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -3,6 +3,9 @@ import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Addcustomer from "./Addcustomer";
+import Editcustomer from "./Editcustomer";
+import Addtraining from "./Addtraining";
 
 
     export default function Customerlist() {
@@ -33,6 +36,48 @@ import Snackbar from '@material-ui/core/Snackbar';
                     .catch(err => console.error(err))
     
             }
+        }
+
+        const editCustomer = (customer, link) => {
+            fetch(link, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(customer)
+            })
+                .then(res => getCustomers())
+                .catch(err => console.log(err))
+        };
+
+        const addCustomer = (customer) => {
+            fetch('https://customerrest.herokuapp.com/api/customers',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(customer)
+                }
+            )
+            .then(_ => getCustomers())
+            .then(_ => {
+                setMsg('New customer added');
+                setOpen(true);
+            })
+            .catch(err => console.error(err))
+        }
+
+        function addTraining(training){
+            fetch('https://customerrest.herokuapp.com/api/trainings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(training)
+            })
+                .then(res => getCustomers())
+                .catch(err => console.log(err))
         }
 
 
@@ -73,11 +118,24 @@ import Snackbar from '@material-ui/core/Snackbar';
                 width: 100,
                 Cell: row => (<Button color="secondary" size="small"
                                       onClick={() => deleteCustomer(row.row._original.links[1].href)}>Delete</Button>)
+            },
+            {
+                filterable: false,
+                sortable: false,
+                width: 100,
+                Cell: row => (<Editcustomer editCustomer={editCustomer} customer={row.row._original}/>)
+            },
+            {
+                filterable: false,
+                sortable: false,
+                width: 100,
+                Cell: row => (<Addtraining addTraining={addTraining} customer={row.row._original}/>)
             }
         ];
     
         return (
             <div>
+                <Addcustomer addCustomer={addCustomer}></Addcustomer>
               <ReactTable defaultPageSize={10} filterable={true} data={customers} columns={columns} sortable={true}/>
             <Snackbar
                 open={open}
